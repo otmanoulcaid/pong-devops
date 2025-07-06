c7248d8f4b79a6fb3f75cc395500e8060cc2543e
otmanoulcaid <ooulcaid@e1r7p9.1337.ma>
2025-07-06 13:23:35 +0100

    init : initial custom logger

---
+++ logger/logger.js
@@ -0,0 +1,65 @@
+import winston from 'winston'
+
+export class Logger
+{
+	constructor(filename, level = 'debug')
+	{
+		this.logger = winston.createLogger({
+			level,
+			format: winston.format.combine(
+				winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+				winston.format.printf(({ timestamp, level, message }) =>
+					`[${timestamp}] ${level.toUpperCase()} - ${message}`
+				)
+			),
+			transports: [
+				new winston.transports.Console(),
+				new winston.transports.File({ filename })
+			]
+		});
+	}
+
+	#formatter(metadata) {
+		const { ip, uri, method, responseTime, statusCode, message } = metadata;
+
+		return `
+			Message: ${message || '?'} - IP: ${ip || '?'} - method: ${method || '?'} - URL: ${uri || '?'} - \
+Response Time: ${responseTime || '?'} - Status Code: ${statusCode || '?'}
+		`.trim();
+	}
+
+	info(metadata)
+	{
+		this.logger.info(this.#formatter(metadata))
+	}
+
+	debug(metadata)
+	{
+		this.logger.debug(this.#formatter(metadata))
+	}
+
+	warn(metadata)
+	{
+		this.logger.warn(this.#formatter(metadata))
+	}
+
+	error(metadata)
+	{
+		this.logger.error(this.#formatter(metadata))
+	}
+
+	fatal(metadata)
+	{
+		this.logger.error(this.#formatter(metadata))
+	}
+
+	trace(metadata)
+	{
+		this.logger.error(this.#formatter(metadata))
+	}
+
+	child()
+	{
+		return this
+	}
+}
