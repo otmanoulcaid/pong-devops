13fac66ddfc2939b3f2f6af7737177809d350314
otmanoulcaid <ooulcaid@e2r6p9.1337.ma>
2025-07-07 14:29:30 +0100

    update : upgrade elasticsearch to 9.0.3

--- elasticsearch/tools/setup.sh
+++ elasticsearch/tools/setup.sh
@@ -2,14 +2,17 @@
 
 cd /home/elasticsearch/
 
-curl https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.3-linux-x86_64.tar.gz > elasticsearch.tar.gz
+curl https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-9.0.3-linux-x86_64.tar.gz > elasticsearch.tar.gz
 
 tar -xvzf /home/elasticsearch/elasticsearch.tar.gz
 
-cat << EOF > /home/elasticsearch/elasticsearch-7.9.3/config/elasticsearch.yml
+cat << EOF > /home/elasticsearch/elasticsearch-9.0.3/config/elasticsearch.yml
 network.host: 0.0.0.0
 http.port: 9200
 discovery.type: single-node
+xpack.security.enabled: false
+xpack.security.http.ssl.enabled: false
+
 EOF
 
-./elasticsearch-7.9.3/bin/elasticsearch
+./elasticsearch-9.0.3/bin/elasticsearch
