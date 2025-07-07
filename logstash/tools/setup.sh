71f0c356c5b2b2127c93707b2de6215f69294b53
otmanoulcaid <ooulcaid@e2r6p9.1337.ma>
2025-07-07 20:21:45 +0100

    update : indexing filter result & elasticsearch ping server

--- logstash/tools/setup.sh
+++ logstash/tools/setup.sh
@@ -6,4 +6,9 @@ curl https://artifacts.elastic.co/downloads/logstash/logstash-9.0.3-linux-x86_64
 
 tar -xvzf logstash.tar.gz
 
+until curl -s http://elasticsearch:9200 > /dev/null; do
+  echo "⏳ En attente d'Elasticsearch..."
+  sleep 5
+done
+
 ./logstash-9.0.3/bin/logstash -f /logstash/logstash.conf
