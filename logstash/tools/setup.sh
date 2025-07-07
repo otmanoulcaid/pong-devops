214e872ff79568585a99042769fa35706448a6af
otmanoulcaid <ooulcaid@e2r6p9.1337.ma>
2025-07-07 11:33:15 +0100

    update : upgrade logstash artifact version

--- logstash/tools/setup.sh
+++ logstash/tools/setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-curl https://artifacts.elastic.co/downloads/logstash/logstash-8.14.0-linux-x86_64.tar.gz > /opt/logstash.tar.gz
+cd /home/logstash/
 
-tar -xvzf /opt/logstash.tar.gz 
+curl https://artifacts.elastic.co/downloads/logstash/logstash-9.0.3-linux-x86_64.tar.gz > logstash.tar.gz
 
-mv logstash-8.14.0 /opt/logstash
-ln -s /opt/logstash/bin/logstash /bin/logstash
+tar -xvzf logstash.tar.gz
 
-
-logstash -f /logstash/logstash.conf
+./logstash-9.0.3/bin/logstash -f /logstash/logstash.conf
